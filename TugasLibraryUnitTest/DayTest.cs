c361ec2c01034c5f9735f33bd651354a0e5e1e40
ASUS <ASUS@LAPTOP-CVSVBJP3>
2021-10-28 15:32:43 +0700

    Add project files.

---
+++ TugasLibraryUnitTest/DayTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class DayTest
+    {
+        private Day day;
+        [SetUp]
+        public void Init()
+        {
+            day = new Day();
+        }
+        [Test]
+        public void TheDayTest()
+        { 
+
+        }
+    }
+}
---
+++ TugasLibraryUnitTest/UserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class UserTest
+    {
+        private User user;
+        [SetUp]
+        public void Init()
+        {
+            user = new User();
+        }
+        [Test]
+        public void TheUserTest()
+        {
+
+        }
+    }
+}
